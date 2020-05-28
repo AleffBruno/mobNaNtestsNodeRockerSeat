e4946a9bb6e9face8bd3be5f856cbf31c6822f99
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-05-28 15:01:48 -0300

    first test, testing userController@store

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 
 const routes = new Router();
 
+import UserController from './app/controllers/UserController';
+
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
 
-routes.post('/users', (req, res) => {
-    return res.json({id:1})
-})
+routes.post('/users', UserController.store)
 
 export default routes;
