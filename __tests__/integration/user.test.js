25c3bd6e8c95b7d9cd42fb0e93608e51b1b7be55
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-05-28 15:15:27 -0300

    first test, testing userController@store

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -12,5 +12,25 @@ describe('User', () => {
             });
 
         expect(response.body).toHaveProperty('id');
+    });
+
+    it(`shouldn't create user with duplicated email `, async () => {
+        await request(app)
+        .post('/users')
+        .send({
+            name: 'Junk',
+            email: 'junk@email.com',
+            password_hash: '123456'
+        });
+
+        const response = await request(app)
+        .post('/users')
+        .send({
+            name: 'Junk',
+            email: 'junk@email.com',
+            password_hash: '123456'
+        });
+
+        expect(response.body).toBe(400);
     })
 })
