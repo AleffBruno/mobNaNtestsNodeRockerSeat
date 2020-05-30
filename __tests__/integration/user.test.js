13333ff1e7bfc8b8d40f8bb33993692c8fa7682f
Aleff Bruno Rodrigues Silva <aleff.silva@secult.local>
2020-05-30 16:18:15 -0300

    add factory-girl and faker

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import factory from '../factories';
 import truncate from '../util/truncate'
 
 describe('User', () => {
@@ -9,34 +11,45 @@ describe('User', () => {
         await truncate();
     })
 
+    it('should encrypt user password when new user created', async () => {
+        const user = await factory.create('User', {
+            password: '123456'
+        });
+
+        const compareHash = await bcrypt.compare("123456", user.password_hash)
+
+        expect(compareHash).toBe(true);
+    })
+
     it('should ne able to register', async () => {
+        const user = await factory.attrs('User');
+        
         const response = await request(app)
             .post('/users')
-            .send({
-                name: 'Junk',
-                email: 'junk@email.com',
-                password_hash: '123456'
-            });
+            .send(user);
 
         expect(response.body).toHaveProperty('id');
     });
 
     it(`shouldn't create user with duplicated email `, async () => {
+        const user = await factory.attrs('User');
+
         await request(app)
         .post('/users')
-        .send({
-            name: 'Junk',
-            email: 'junk@email.com',
-            password_hash: '123456'
-        });
+        .send(user);
 
         const response = await request(app)
         .post('/users')
-        .send({
-            name: 'Junk',
-            email: 'junk@email.com',
-            password_hash: '123456'
-        });
+        .send(user);
+
+        // DEIXEI AQUI PARA SERVIR DE EXEMPLO...
+        // const response = await request(app)
+        // .post('/users')
+        // .send({
+        //     name: 'Junk',
+        //     email: 'junk@email.com',
+        //     password: '123456'
+        // });
 
         expect(response.status).toBe(400);
     })
